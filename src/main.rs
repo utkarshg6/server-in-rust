0aef5240aa370e8c64f1ebbb657e4d5b41cfbf6c
utkarsh <utkarsh@indorse.io>
2021-08-15 16:08:50 +0530

    feat: add struct Request and enum Method

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -15,4 +20,22 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr)
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: String,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
