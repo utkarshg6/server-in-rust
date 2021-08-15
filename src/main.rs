5a345f1f54d047e24d3d0da9d9a7d9871a381a4c
utkarsh <utkarsh@indorse.io>
2021-08-15 16:43:22 +0530

    feat: modularize code

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,48 @@
-fn main() {
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
+use http::request::Request;
+use server::Server;
 
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr)
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr)
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: String,
-}
+mod http {
+    pub mod request {
+        use super::method::Method;
 
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
