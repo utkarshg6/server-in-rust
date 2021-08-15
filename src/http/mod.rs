42ba55d776363506c08f4dc4af335db2e20ad659
utkarsh <utkarsh@indorse.io>
2021-08-15 17:06:24 +0530

    refactor: modularize into different files and folders

---
+++ src/http/mod.rs
@@ -0,0 +1,7 @@
+// This file exposes submodules or entities inside of them.
+
+pub use method::Method; // Exposing only enum Method
+pub use request::Request; // Exposing only struct Request
+
+pub mod method; // Exposing the whole submodule method
+pub mod request;
