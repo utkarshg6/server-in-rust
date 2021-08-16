1e778c60795104307de6a4413e11f2e2acad6d17
utkarsh <utkarsh@indorse.io>
2021-08-16 20:33:39 +0530

    feat: add loop for listening the incoming tcp connection

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -8,6 +10,20 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
+
+        // If the result is OK, it'll unwrap the result into variable
+        // otherwise it'll stop the program from executing
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            let res = listener.accept();
+
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
+        }
     }
 }
