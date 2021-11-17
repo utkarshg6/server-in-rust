63c6c7797dba92912b1a096cceec49a8ce2422a8
utkarsh <utkarsh@indorse.io>
2021-11-17 11:26:41 +0530

    feat: advanced error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,3 +1,5 @@
+use std::str;
+use std::str::Utf8Error;
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
@@ -21,6 +23,9 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+
+        let request = str::from_utf8(buf)?;
+
         unimplemented!()
     }
 }
@@ -35,11 +40,17 @@ pub enum ParseError {
 impl ParseError {
     fn message(&self) -> &str {
         match(self) {
-            Self::InvalidRequest => "InvalidRequest"
-            Self::InvalidEncoding => "InvalidEncoding"
-            Self::InvalidProtocol => "InvalidProtocol"
-            Self::InvalidMethod => "InvalidMethod"
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidMethod => "InvalidMethod",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
