c82681af152eb8c6f601a789a293d54f7986d0d1
utkarsh <utkarsh@indorse.io>
2021-11-17 17:03:52 +0530

    feat: iterating over strings - define fn get_next_word

--- src/http/request.rs
+++ src/http/request.rs
@@ -30,6 +30,17 @@ impl TryFrom<&[u8]> for Request {
     }
 }
 
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i+1..]))
+        }
+    }
+
+    unimplemented!()
+}
+
 pub enum ParseError {
     InvalidRequest,
     InvalidEncoding,
