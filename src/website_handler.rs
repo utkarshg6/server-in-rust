24b44a5c00017e2d4ff5db8fec00d4b24c3ac203
Utkarsh Gupta <utkarshg6e@gmail.com>
2021-12-07 22:27:37 +0530

    feat: routing incoming requests - add routes and retrieve GET request

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,22 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(
-            StatusCode::Ok,
-            Some("<h1>Hooray! It's fuckin working!</h1>".to_string())
-        )
+        // Response::new(
+        //     StatusCode::Ok,
+        //     Some("<h1>Hooray! It's fuckin working!</h1>".to_string())
+        // )
+
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
