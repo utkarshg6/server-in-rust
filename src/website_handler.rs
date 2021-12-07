93e0744e1fcc98b74c5f80d37354aced6e98832b
Utkarsh Gupta <utkarshg6e@gmail.com>
2021-12-08 02:16:56 +0530

    feat: serving arbitrary files securely - canonicalize file path and log directory traversal attacks

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,7 +13,18 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Direct Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
+        // fs::read_to_string(path).ok()
     }
 }
 
@@ -28,7 +39,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None)
+                },
             },
             _ => Response::new(StatusCode::NotFound, None)
         }
