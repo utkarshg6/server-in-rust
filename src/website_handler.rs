d37c11b16c1b46cf3bdfe348fcb26196ff94863e
Utkarsh Gupta <utkarshg6e@gmail.com>
2021-12-08 01:39:51 +0530

    feat: serving html files - read a file into string

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::server::Handler;
 use super::http::{Method, Request, Response, StatusCode};
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String
@@ -8,6 +9,11 @@ pub struct WebsiteHandler {
 impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self {public_path}
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
     }
 }
 
@@ -20,7 +26,7 @@ impl Handler for WebsiteHandler {
 
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
